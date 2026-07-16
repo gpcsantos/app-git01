f705099f7626b5eb53935887cf9081d7a1d3e66c
joaovitor2327 <gamefran2012@gmail.com>
2026-07-16 10:55:35 -0300

    Header e Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <>
+      <Header />
       <div className="p-5 bg-cyan-950 text-gray-200 flex justify-center ">
         <h1 className="text-2xl font-bold">Projeto BASE</h1>
       </div>
+      <Footer />
     </>
   );
 }
