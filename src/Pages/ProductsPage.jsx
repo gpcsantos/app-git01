d5e3adfb4b1fe8cb252d54d7e1799fc4e379a423
JonathanDamascenoBR <jonathanvdamasceno@gmail.com>
2026-07-16 11:23:44 -0300

    Correção

--- src/Pages/ProductsPage.jsx
+++ src/Pages/ProductsPage.jsx
@@ -2,7 +2,7 @@ import Products from "../Components/Products"
 
 function ProductsPage() {
     return <div className="bg-slate-950 flex flex-col items-center justify-start min-h-screen">
-        <h1 className="text-4xl text-white font-bold my-6">Página de Produtos</h1>
+        <h1 className="text-4xl text-white font-bold  my-6">Página de Produtos</h1>
         <div className="flex gap-4 mb-4">
             <Products
                 title={"Produto 01"}
