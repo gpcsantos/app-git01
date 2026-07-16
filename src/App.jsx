8bfa91ce646e3a1a45ca055973c3f8dd0140d8e5
JonathanDamascenoBR <jonathanvdamasceno@gmail.com>
2026-07-16 11:13:41 -0300

    Revert "Primeiro commit página de produto"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import ProductsPage from "./Pages/ProductsPage";
-
 function App() {
   return (
     <>
       <div className="p-5 bg-cyan-950 text-gray-200 flex justify-center ">
         <h1 className="text-2xl font-bold">Projeto BASE</h1>
       </div>
-      <ProductsPage />
     </>
   );
 }
