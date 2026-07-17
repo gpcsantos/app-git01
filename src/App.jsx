e50c890e50a89d008241774137c411f165fe1e1e
JULIO CESAR DE ARAUJO BARBOSA <julio.cabarbosa@senacsp.edu.br>
2026-07-17 11:35:54 -0300

    page de produtos

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./Pages/HomePage";
+import ProductsPage from "./Pages/ProductsPage"; // Import da página de produtos
 import "./index.css";
 
 function App() {
   return (
-    <div className="min-h-screen bg-slate-900 text-slate-100 flex flex-col justify-between">
-      {/* 1. Header Fixo no Topo */}
-      <Header />
+    <BrowserRouter>
+      <div className="min-h-screen bg-slate-900 text-slate-100 flex flex-col justify-between">
+        {/* Header Fixo no Topo */}
+        <Header />
 
-      {/* 2. Conteúdo Central (com espaçamento pt-20 e pb-20 para não ficar debaixo do Header/Footer) */}
-      <main className="flex-1 pt-20 pb-20 flex flex-col items-center w-full">
-        
-        {/* Banner 'Projeto BASE' */}
-        <div className="p-5 bg-cyan-950 text-gray-200 flex justify-center w-full shadow-md">
-          <h1 className="text-2xl font-bold">Projeto BASE</h1>
-        </div>
+        {/* Conteúdo Central */}
+        <main className="flex-1 pt-20 pb-20 flex flex-col items-center w-full">
+          {/* Banner 'Projeto BASE' */}
+          <div className="p-5 bg-cyan-950 text-gray-200 flex justify-center w-full shadow-md">
+            <h1 className="text-2xl font-bold">Projeto BASE</h1>
+          </div>
 
-        {/* Página Principal dos Produtos */}
-        <HomePage />
-      </main>
+          {/* Gerenciamento das Rotas do Projeto */}
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/produtos" element={<ProductsPage />} />
+          </Routes>
+        </main>
 
-      {/* 3. Footer Fixo no Fundo */}
-      <Footer />
-    </div>
+        {/* Footer no Fundo */}
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import ProductsPage from "../../Pages/ProductsPage";
 
 function Header() {
   return (
