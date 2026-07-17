12e944a1b901b4b73ef99ee40cabf4ccd9b2bc5c
Julio1215 <162897574+Julio1215@users.noreply.github.com>
2026-07-17 13:25:11 +0000

    Correção de pastas

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import HomePage from "./Components/HomePage"
+import HomePage from "./Pages/HomePage";
 import "./index.css";
+
 function App() {
   return (
-    <div className="home">
-      <h1>Home</h1>
-    </div>
+    <>
+      <Header />
+      
+      <div className="p-5 bg-cyan-950 text-gray-200 flex justify-center">
+        <h1 className="text-2xl font-bold">Projeto BASE</h1>
+      </div>
+
+      <HomePage />
+      
+      <Footer />
+    </>
   );
 }
 
 export default App;
-    <>
-      <Header />
-      <div className="p-5 bg-cyan-950 text-gray-200 flex justify-center ">
-        <h1 className="text-2xl font-bold">Projeto BASE</h1>
-      </div>
-      <Footer />
-      <HomePage />
-    </>
-  )
-}
